9da1f00dbd87f810e7120e4acc59bce71d75dbcc
YOUR NAME <YOUR@EMAIL.com>
2021-05-13 06:42:47 -0400

    generic

--- GenericDemo/src/example/KVPUtilities.java
+++
@@ -1,9 +0,0 @@
-package example;
-
-public class KVPUtilities {
-	public static <K,V> boolean equality(KeyValuePair<K,V> p1, KeyValuePair<K,V> p2) {
-		return p1.getKey().equals(p2.getKey()) && 
-			   p1.getValue().equals(p2.getValue());	
-	}
-
-}
