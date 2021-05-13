6e2fba83b785f4c9952f05a4525ec3649fdc55de
YOUR NAME <YOUR@EMAIL.com>
2021-05-13 06:46:53 -0400

    Lab-Exception

--- Lab-Exception/src/com/sekou/exceptions/EvenNumberException.java
+++
@@ -1,9 +0,0 @@
-package com.sekou.exceptions;
-
-public class EvenNumberException extends Exception{
-	@Override
-    public String getMessage() {
-        return "You cannot input an even number.";
-    }
-
-}
